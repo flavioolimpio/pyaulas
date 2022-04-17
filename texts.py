62c6f9e9d704289d57d425e3051a81d83a73d7d7
Mphyschem <>
2022-04-17 14:40:26 -0300

    update text

--- texts.py
+++ texts.py
@@ -4,13 +4,14 @@ class Texts:
 
     def text1(self):
         TEXT1 = """
-        Devido a pandemia ocasionado pelo novo conoravírus (COVID-19), uma reestruturação do ensino teve que ser adotado no Brasil e no mundo para mitigar a transmissão do vírus. 
+        <body style='text-align: justify; color: black;'>
+        <p> Devido a pandemia ocasionado pelo novo conoravírus (COVID-19), uma reestruturação do ensino teve que ser adotado no Brasil e no mundo para mitigar a transmissão do vírus. 
         Vários estudos já têm indicado o prejuízo que a pandemia está provocando e irá provocar nos próximos anos no sistema educaional mundial e brasileiro. Neste sentido, é de suma importância, 
         que os professores, apoiados por pesquisas cientificas, utilizem metodologias alternativas para o melhor aprendizado do aluno em sala de aula. 
-
-        Foi com este pensamento que o curso de Química Geral do 1° semestre de 2022 foi repensado. Este site foi desenvolvido com o objetivo de guiar o aluno no curso de química gearal ministrada na Universidade Federal de Goiás pelo Prof. Msc. Flávio Olimpio Sanches Neto. 
+        Foi com este pensamento que as disciplinas ofercidos pelo Prof. Me. Flávio Olimpio foram repensados. Este site foi desenvolvido com o objetivo de guiar o aluno no curso de uma disciplina de química ministrada na Universidade Federal de Goiás pelo Prof. Me. Flávio Olimpio Sanches Neto. 
         A ideia é que o site se aperfeiçoe com o tempo e com o feedback dos alunos. 
-        
+        </p>
+        </body>
         """
 
         return TEXT1
@@ -18,35 +19,32 @@ class Texts:
     def text2(self):
         TEXT2 = """
         ## Informação do curso  
+        <body style='text-align: justify; color: black;'>
+        <p> As disciplinas ofericidas no curso de verão e que poderão ser acessados pelos alunos são:</p>        
+
+        <p> (i)  INQ0322 - Química Geral; </p>
+        <p> (ii) INQ0330 - Físico-Química. </p>
         
-        Este curso é uma introdução à química geral. Os tópicos incluem princípios de estrutura, ligação química, reações químicas e reatividade química . 
-
-        Aula 1 - Estrutura Atômica: átomos multieletrônicos;                                                                 
-        Aula 2 - Ligação Química e Geometria Molecular;                                                                
-        Aula 3 - Estequiometria;                                                                                    
-        Aula 4 - Reações em meio aquoso;                                                                             
-        Aula 5 - Propriedade dos Gases;                                                                                      
-        Aula 6 - 1ª Lei da Termodinâmica;                                                                              
-        Aula 7 - 2ª Lei da Termodinâmica;                                                                           
-        Aula 8 - Eletroquímica;                                                                             
-        Aula 9 - Equilíbrio Químico;                                                                             
-        Aula 10 - Cinética Química.                                         
-
+        
+        <p>As demais informações como ementa e detalhes adicionais podem ser acessado no menu ao lado.</p>
+        </body>
         """
 
         return TEXT2
     
     def text3(self):
         TEXT3 = """
-        ## Informações adicionais:                                           
-        Msc. Flávio Olimpio Sanches Neto (Universidade Estadual de Goiás - UEG)                             
-        Doutorando em Química (Universidade de Brasília - UnB)                    
-        Professor Substituto no IQ-UFG (Instituto de Química - Universidade Federal de Goiás)                                                        
-        http://lattes.cnpq.br/3756571652061846                                                      
-        https://www.linkedin.com/in/flavioosneto/                               
-        https://www.researchgate.net/profile/Flavio-Neto-3           
-        flavio_olimpio@outlook.com
-
+        ## Informações adicionais: 
+        <body style='text-align: justify; color: black;'>   
+        <p>Flávio Olimpio Sanches Neto</p>
+        <p>Químico Industrial e Mestre em química (Universidade Estadual de Goiás - UEG)</p>
+        <p>Doutorando em Química (Universidade de Brasília - UnB)</p>
+        <p>Professor Substituto no IQ-UFG (Instituto de Química - Universidade Federal de Goiás)
+        <p> <a href="http://lattes.cnpq.br/3756571652061846"target="_blank">Lattes</a>. </p>
+        <p> <a href="https://www.linkedin.com/in/flavioosneto/"target="_blank">Linkedin</a>. </p>
+        <p> <a href="https://www.researchgate.net/profile/Flavio-Neto-3"target="_blank">Linkedin</a>. </p>
+        <p>flavio_olimpio@outlook.com</p>
+        </body>
         """
 
         return TEXT3
