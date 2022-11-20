93ce4e76f0513ee825af069b4098b79bc4202f66
flavioolimpio <34543135+flavioolimpio@users.noreply.github.com>
2022-11-20 07:15:13 -0300

    Add files via upload
    ifg_2

--- texts.py
+++ texts.py
@@ -7,8 +7,8 @@ class Texts:
         <p style='text-align: justify; color: black;'> Devido a pandemia ocasionado pelo novo coronavírus (COVID-19), uma reestruturação do ensino teve que ser adotado no Brasil e no mundo para mitigar a transmissão do vírus. 
         Vários estudos já têm indicado o prejuízo que a pandemia está provocando e irá provocar nos próximos anos no sistema educacional mundial e brasileiro.
         Neste sentido, é de suma importância, que os professores, apoiados por pesquisas cientificas, utilizem metodologias alternativas para o melhor aprendizado do aluno em sala de aula. 
-        Foi com este pensamento que as disciplinas oferecidas pelo Prof. Me. Flávio Olimpio foram repensadas.
-        Este site foi desenvolvido com o objetivo de guiar os alunos nas disciplinas de química ministradas na Universidade Federal de Goiás pelo Prof. Me. Flávio Olimpio Sanches Neto. 
+        Foi com este pensamento que as disciplinas oferecidas pelo Prof. Dr. Flávio Olimpio foram repensadas.
+        Este site foi desenvolvido com o objetivo de guiar os alunos nas disciplinas de química ministradas na Instituto Federal de Goiás - Câmpus Uruaçu pelo Prof. Dr. Flávio Olimpio Sanches Neto. 
         A ideia é que o site se aperfeiçoe com o tempo e com os feedbacks dos alunos.</p>
         """
 
@@ -18,10 +18,9 @@ class Texts:
         TEXT2 = """
         ## Informação do curso  
         <p style='text-align: justify; color: black;'>
-        As disciplinas ofericidas no curso de verão e que poderão ser acessados pelos alunos são:<br>        
-        (i)  INQ0158 - Química Geral B;  <br>
-        (ii) INQ0324 - Química Geral Experimental. <br>
-        (iii) INQ0331 - Físico-Química Experimental. <br>
+        As disciplinas ofericidas no curso e que poderão ser acessados pelos alunos são:<br>        
+        (i)  Química Geral I;  <br>
+        (ii) Química Geral Experimental. <br>
         As demais informações como ementa e detalhes adicionais das disciplinas podem ser acessadas no menu ao lado.
         </p>
         """
@@ -34,9 +33,10 @@ class Texts:
         <p style='text-align: justify; color: black;'>   
         Flávio Olimpio Sanches Neto<br>
         Químico Industrial e Mestre em química (Universidade Estadual de Goiás - UEG)<br>
-        Doutorando em Físico-Química (Universidade de Brasília - UnB)<br>
-        Professor Substituto no IQ-UFG (Instituto de Química - Universidade Federal de Goiás)<br>
-        flavio_olimpio@ufg.br<br>
+        Doutor em Físico-Química (Universidade de Brasília - UnB)<br>
+        Pós doutorando em Química (Instituto de Química - Universidade Federal de Goiás)<br>
+        Professor IFG - Uruaçu <br>
+        flavio_olimpio@outlook.com<br>
         <a href="http://lattes.cnpq.br/3756571652061846"target="_blank">Lattes</a><br>
         <a href="https://www.linkedin.com/in/flavioosneto/"target="_blank">Linkedin</a><br>
         <a href="https://www.researchgate.net/profile/Flavio-Neto-3"target="_blank">ResearchGate</a><br>
